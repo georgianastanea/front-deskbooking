9d9bdf50e9005d291517da17a8700fc98fa53e1d
georgianastanea9 <109727380+georgianastanea9@users.noreply.github.com>
2022-11-25 13:39:56 +0200

    initial

---
+++ src/pages/AddUserPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+
+function AddUserPage() {
+
+    // const [users, setPipelines] = useState([]);
+
+    return (
+        <div>these are the users</div>
+    )
+}
+
+export default AddUserPage
---
+++ src/routes/routes.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Routes as ReactRoutes } from 'react-router-dom'
+import HomePage from '../pages/HomePage'
+import AddUserPage from '../pages/AddUserPage'
+
+
+
+const Routes = () => {
+    return (
+        <ReactRoutes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/users" element={<AddUserPage />} />
+            {/* <Route path="/offices" element={<OfficesPage />} /> */}
+        </ReactRoutes>
+    )
+}
+
+export default Routes
